dfdcf5f118ce3a50e40b2d82eea121dd5d453dcf
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2019-12-02 10:37:49 -0300

    Set job status

--- arrebol/services/scheduler.mjs
+++ arrebol/services/scheduler.mjs
@@ -4,19 +4,20 @@ import request from 'request';
 
 function execute(task, worker, queue) {
     worker.busy = true;
-    // const job = await Job.findById(task.jobId);
     
     Task.updateOne({
         _id: task._id,
         state: {
             $eq: 'READY'
         }
-    }, {state: 'RUNNING'}, { runValidators: true }).then((response) => {
+    }, {state: 'RUNNING'}, { runValidators: true }).then(async response => {
         if (response.nModified == 0) {
             worker.busy = false;
             schedule(queue);
             return;
         }
+
+        await Job.updateOne({ _id: task.jobId, state: { $eq: 'READY' }}, { state: 'RUNNING' }, { runValidators: true });
 
         request.post({
             url: `${worker.address}/worker/run-task`,
@@ -32,7 +33,13 @@ function execute(task, worker, queue) {
             await Task.updateOne({
                 _id: task._id
             }, {state: 'FINISHED'}, { runValidators: true });
-    
+
+            const unFinishedTasks = await Task.find({ jobId: task.jobId, state: { $ne: 'FINISHED' } }).countDocuments();
+
+            if (unFinishedTasks === 0) {
+                await Job.updateOne({ _id: task.jobId }, { state: 'FINISHED' }, { runValidators: true });
+            }
+
             worker.busy = false;
             schedule(queue);
         });
