554b1d45019f5a7ce7fd92daa8d36aa108fc8f72
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2019-12-01 23:11:15 -0300

    Refact function execute of scheduler

--- services/scheduler.mjs
+++ services/scheduler.mjs
@@ -2,29 +2,29 @@ import Task from '../models/task.mjs';
 import Job from '../models/job.mjs';
 import request from 'request';
 
-async function execute(task, worker, queue) {
+function execute(task, worker, queue) {
     worker.busy = true;
     // const job = await Job.findById(task.jobId);
     
     task.state = "RUNNING";
-    await task.save();
-
-    request.post({
-        url: `${worker.address}/worker/run-task`,
-        body: JSON.stringify({ task: task._id }),
-        headers: {
-            'Content-Type': 'application/json'   
-        }
-    }, async (error, response, body) => {
-        if (error) {
-            console.log(error);
-        }
-        
-        task.state = "FINISHED";
-        await task.save();
-
-        worker.busy = false;
-        schedule(queue);
+    task.save().then(() => {
+        request.post({
+            url: `${worker.address}/worker/run-task`,
+            body: JSON.stringify({ task: task._id }),
+            headers: {
+                'Content-Type': 'application/json'   
+            }
+        }, async (error, response, body) => {
+            if (error) {
+                console.log(error);
+            }
+    
+            task.state = "FINISHED";
+            await task.save();
+    
+            worker.busy = false;
+            schedule(queue);
+        });
     });
 }
 
@@ -34,10 +34,9 @@ function schedule(queue) {
     tasks.forEach(async taskId => {
         const task = await Task.findById(taskId);
 
-        queue.workers.forEach(async worker => {
+        queue.workers.forEach(worker => {
             if (task.state == 'READY' && !worker.busy) {
-                task.state = 'RUNNING';
-                await execute(task, worker, queue);
+                execute(task, worker, queue);
                 return task;
             }
         });
